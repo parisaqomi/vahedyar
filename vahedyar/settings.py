4fa53a9be5138ed88841a0431daa9ace00bde872
parisaqomi <parisaq97@yahoo.com>
2019-05-13 03:49:44 +0430

    dotvscode

--- vahedyar/settings.py
+++ vahedyar/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #'material',
+    #'material.admin',
+    'material_admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
