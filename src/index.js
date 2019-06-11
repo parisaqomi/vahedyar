35a81bf41bcdb15dcc8bb2a4dae746d88aa6f5f1
parisaqomi <parisaq97@gmail.com>
2019-06-11 10:22:29 +0430

    delete extras

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch } from "react-router-dom";
-// import { Router, Route, Switch, Redirect } from "react-router-dom";
-
 // core components
-import Admin from "layouts/Admin.jsx";
 import RTL from "layouts/RTL.jsx";
 import Login from "layouts/Login.jsx";
 
@@ -17,9 +14,7 @@ ReactDOM.render(
   <Router history={hist}>
     <Switch>
       <Route path="/rtl" component={RTL} />
-      <Route path="/admin" component={Admin} />
       <Route path="/" component={Login} />
-      {/* <Redirect from="/" to="/admin/dashboard" /> */}
     </Switch>
   </Router>,
   document.getElementById("root")
--- src/views/RTLPage/RTLPage.jsx
+++ src/views/RTLPage/RTLPage.jsx
@@ -1,38 +1,16 @@
-/*eslint-disable*/
 import React from "react";
 import PropTypes from "prop-types";
-// react plugin for creating charts
 import ChartistGraph from "react-chartist";
-// @material-ui/core
 import withStyles from "@material-ui/core/styles/withStyles";
 import Icon from "@material-ui/core/Icon";
-// @material-ui/icons
-// import Store from "@material-ui/icons/Store";
-// import Warning from "@material-ui/icons/Warning";
-// import DateRange from "@material-ui/icons/DateRange";
-// import LocalOffer from "@material-ui/icons/LocalOffer";
-// import Update from "@material-ui/icons/Update";
-// import ArrowUpward from "@material-ui/icons/ArrowUpward";
-// import AccessTime from "@material-ui/icons/AccessTime";
 import Accessibility from "@material-ui/icons/Accessibility";
-// import BugReport from "@material-ui/icons/BugReport";
-// import Code from "@material-ui/icons/Code";
-// import Cloud from "@material-ui/icons/Cloud";
-// core components
-//import Button from "components/CustomButtons/Button.jsx";
 import GridItem from "components/Grid/GridItem.jsx";
 import GridContainer from "components/Grid/GridContainer.jsx";
-// import Table from "components/Table/Table.jsx";
-// import Tasks from "components/Tasks/Tasks.jsx";
-// import CustomTabs from "components/CustomTabs/CustomTabs.jsx";
-// import Danger from "components/Typography/Danger.jsx";
 import Card from "components/Card/Card.jsx";
 import CardHeader from "components/Card/CardHeader.jsx";
-//import CardAvatar from "components/Card/CardAvatar.jsx";
 import CardIcon from "components/Card/CardIcon.jsx";
 import CardBody from "components/Card/CardBody.jsx";
 import CardFooter from "components/Card/CardFooter.jsx";
-//import SnackbarContent from "components/Snackbar/SnackbarContent.jsx";
 
 import {
   dailySalesChart,
